60eb0cbda434c576a50ca12410fb989d181bc9c1
AbdulkareemPaehohhilay <6010110402@psu.ac.th>
2019-09-19 11:40:28 +0700

    commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import WordCard from './WordCard';
+//import CharacterCard from './CharacterCard';
+//import './App.css';
+//const word = "Hello";
+
+class App extends Component {
+    render() {
+        return (
+        <div>
+        <WordCard value="kareem"/>
+        </div>
+        );
+       }
+}
+export default App;
